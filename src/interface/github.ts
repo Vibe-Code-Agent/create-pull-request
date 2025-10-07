d108bf5f46d07ac318697b4d2f7b71eab7d19bdc
Doraemon <bradley.nguyen@employmenthero.com>
2025-10-07 21:00:13 +0700

    Optimize interface

--- src/interface/github.ts
+++ src/interface/github.ts
@@ -1,17 +1,17 @@
 export interface GitHubRepo {
-  owner: string;
-  repo: string;
+    owner: string;
+    repo: string;
 }
 
 export interface PullRequest {
-  title: string;
-  body: string;
-  head: string;
-  base: string;
-  draft?: boolean;
+    title: string;
+    body: string;
+    head: string;
+    base: string;
+    draft?: boolean;
 }
 
 export interface PullRequestTemplate {
-  name: string;
-  content: string;
+    name: string;
+    content: string;
 }
